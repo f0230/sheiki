35b4c196c4a4709331b5b9770a5bcd6bd6540598
f0230 <fran.salisg@gmail.com>
2025-05-14 00:42:23 -0300

    first commit

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js';
 
 // Asegúrate de que estas credenciales estén actualizadas
 const supabaseUrl = process.env.SUPABASE_URL;  // Vercel maneja estas variables de entorno
-const supabaseKey = process.env.SUPABASE_KEY;  // Vercel maneja estas variables de entorno
+const supabaseKey = process.env.SUPABASE_ANON_KEY;  // Vercel maneja estas variables de entorno
 export const supabase = createClient(supabaseUrl, supabaseKey);
