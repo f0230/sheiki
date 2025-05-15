a92f2f704b483d08f281fe31016d2251319340ae
f0230 <fran.salisg@gmail.com>
2025-05-14 23:00:03 -0300

    Fix: import explícito de Preference para ESM

--- api/create-preference.js
+++ api/create-preference.js
@@ -1,4 +1,5 @@
-import { MercadoPagoConfig, Preference } from 'mercadopago';
+import { MercadoPagoConfig } from 'mercadopago';
+import { Preference } from 'mercadopago/dist/clients/preference/create';
 
 const mp = new MercadoPagoConfig({
     accessToken: process.env.MP_ACCESS_TOKEN,
