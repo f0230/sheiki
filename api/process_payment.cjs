7420bd357dde05d517b672059b13f896cd021d9a
f0230 <fran.salisg@gmail.com>
2025-05-14 21:41:15 -0300

    Elimino archivos .cjs duplicados que causaban conflicto

--- api/process_payment.cjs
+++
@@ -1,4 +0,0 @@
-// Este archivo no se necesita si usás <Payment /> directamente
-module.exports = (req, res) => {
-    return res.status(200).json({ message: 'Sin uso actual: se maneja todo desde Payment Brick' });
-};
